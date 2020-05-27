63c3ff5649c4f2722d1de422c0938b1c373038f2
bobby-brice <bobby.brice@gmail.com>
2020-05-27 22:53:21 +0000

    completed findKeysByValue

---
+++ findKeyByValue.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(bestTVShowsByGenre, value) {
+  for (const prop in bestTVShowsByGenre) {
+    if (bestTVShowsByGenre.hasOwnProperty(prop)) {
+      if (bestTVShowsByGenre[prop] === value) {
+        return prop;
+      }
+    }
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+console.log(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"));
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
