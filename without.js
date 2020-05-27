8e1fb06c2fca569d7d3120eae7aa651a3c7a6439
bobby-brice <bobby.brice@gmail.com>
2020-05-27 00:10:47 +0000

    adding without.js

---
+++ without.js
@@ -0,0 +1,40 @@
+//return a subset of a given array, removing unwanted elements
+//without([1, 2, 3], [1]) // => [2, 3]
+//without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
+const without = function(source, itemsToRemove) {
+  console.log("source:" + source, "removes:" + itemsToRemove);
+  return source.filter(i => !itemsToRemove.includes(i));
+};
+
+// console.log(without([1, 2, 3], [1])); // => [2,3]
+// console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
+
+const isEqual = function(arr1, arr2) {
+  let result = true;
+  if (arr1.length !== arr2.length) {
+    result = false;
+  }
+
+  for (let i = 0; i < arr1.length && result; i++) {
+    if (arr1[i] !== arr2[i]) {
+      result = false;
+    }
+  }
+  return result;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  const result = isEqual(arr1, arr2);
+  if (result) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+assertArraysEqual(
+  without([1,2,3], [1]),
+  [2,3]
+);
+
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
