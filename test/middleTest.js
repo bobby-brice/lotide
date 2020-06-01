8dbaec798ba6fa37fa0600c2fdcf1e328de36192
bobby-brice <bobby.brice@gmail.com>
2020-06-01 20:53:42 +0000

    refactored middle.js to use mocha tests

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,17 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-assertArraysEqual(middle([1]), []); // => []
-assertArraysEqual(middle([1, 2]), []); // => []
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
+describe("#middle", () => {
+  it("arrays with one or two elements, there is no middle. Return an empty array. []", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+});
+
+it(" arrays with odd number of elements, an array containing a single middle element should be returned - middle([1, 2, 3, 4, 5]) // => [3]", () => {
+  assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+});
+
+it("arrays with an even number of elements, an array containing the two elements in the middle should be returned - [1, 2, 3, 4, 5, 6]) => [3, 4]", () => {
+  assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+});
+
