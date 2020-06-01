1d5a88dec423cee5aca42bb6077b9e5d67863ec2
bobby-brice <bobby.brice@gmail.com>
2020-06-01 17:47:53 +0000

    refactored middle fn and moved test cases to a separate test file

--- middle.js
+++ middle.js
@@ -1,25 +1,3 @@
-const isEqual = function(arr1, arr2) {
-  let result = true;
-  if (arr1.length !== arr2.length) {
-    result = false;
-  }
-
-  for (let i = 0; i < arr1.length && result; i++) {
-    if (arr1[i] !== arr2[i]) {
-      result = false;
-    }
-  }
-  return result;
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  const result = isEqual(arr1, arr2);
-  if (result) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
 //The middle function should return an array with only the middle element(s) of the provided array. This means that the length of the returned elements could vary.
 
 //For arrays with one or two elements, there is no middle. Return an empty array.
@@ -45,7 +23,5 @@ const middle = function(arr) {
   return newArr;
 };
 
-assertArraysEqual(middle([1]), []);// => []
-assertArraysEqual(middle([1, 2]), []);// => []
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);// => [3, 4]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);// => [3]
+module.exports = middle;
+
