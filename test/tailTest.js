6caee133ea126a152da1a07976bc2ee5b7e2c2bc
bobby-brice <bobby.brice@gmail.com>
2020-06-01 18:36:06 +0000

    refactored tail.js to use mocha test assertions through deep equality

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,13 +1,17 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("#tail", () => {
+  it("returns each item after the first index in the array ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Hello', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
+  });
+
+  it("returns 2 for the length of the array after slice", () => {
+    assert.deepEqual(tail(['Hello', 'Lighthouse', 'Labs']).length , 2);
+  });
+
+  it("returns 'Lighthouse' as the first index for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Hello', 'Lighthouse', 'Labs'])[0], 'Lighthouse');
+  });
+});
