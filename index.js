8a02f4101654db404adcc9b19f8c856d46bc9d84
bobby-brice <bobby.brice@gmail.com>
2020-06-01 21:26:56 +0000

    added the remaining utility functions to index.js and stored them in an object to be easily exported

---
+++ index.js
@@ -0,0 +1,31 @@
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const without = require('./without');
+
+
+
+module.exports = {
+  head,
+  tail,
+  middle,
+  countLetters,
+  countOnly,
+  findKey,
+  findKeyByValue,
+  letterPositions,
+  map,
+  takeUntil,
+  eqArrays,
+  eqObjects,
+  without
+};
