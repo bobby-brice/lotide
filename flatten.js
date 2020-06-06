93318fa277be409286721a7ba4d0a8829988ac3e
Bobby Brice <bobby.brice@gmail.com>
2020-06-06 14:27:23 -0400

    added flatten.js function from stretch

--- flatten.js
+++ flatten.js
@@ -1,8 +1,18 @@
 //Create a function flatten which will take in an array containing elements including nested arrays of elements, and return a "flattened" version of the array.
 
-const flatten = function() {
+const flatten = function(arr) {
+  const result = [];
 
+  arr.forEach((i) => {
+    if (Array.isArray(i)) {
+      result.push(...flatten(i));
+    } else {
+      result.push(i);
+    }
+  });
+
+  return result;
 };
 
-flatten([1, 2, [3, 4], 5, [6]]);
+console.log(flatten([1, 2, [3, 4], 5, [6]]));
 // => [1, 2, 3, 4, 5, 6]
